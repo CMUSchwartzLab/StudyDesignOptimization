195db6496eeec1be806914985fcbf7ac5b9ef602
arjunsrivatsa <arjunsrivatsa11@gmail.com>
2024-06-25 17:52:56 -0400

    Add files via upload

--- generate_test_results.py
+++ generate_test_results.py
@@ -1,5 +1,6 @@
+import seaborn as sns
 from liquid_biopsyopt import *
-'''
+
 path_to_train_matrix = '/projects/schwartzlabscratch/DesignOpt/test_results/adjustpaired3/allX3.txt'
 liquid_bio_source_dir = '/projects/schwartzlabscratch/LiquidBioData/test_data'
 ground_truth_dir = '/projects/schwartzlabscratch/LiquidBiopsyData/test_data/results'
@@ -9,6 +10,17 @@ def loadTestPoints(X_final):
   X_withscore = np.loadtxt(X_final)
   X = X_withscore[:-1]
   return X
+
+def generate_and_save_plot(X, fname):
+  coverage = X[:,0]
+  num_samples = X[:,6]
+  score = X[:,7]
+  plot = sns.scatterplot(x = coverage, y = num_samples, hue =score)
+  plot.set_title("Coverage vs Number of Samples colored by Overall Score")
+  plot.set_xlabel("Coverage")
+  plot.set_ylabel("Number of Samples")
+  figure = plot.get_figure()
+  figure.savefig(output_dir + '/'+fname+ '.png')
 
 X = loadTestPoints(path_to_train_matrix)
 lamb = 0
@@ -23,9 +35,9 @@ total_loss = np.array(total_loss)
 scores = np.reshape(total_loss, (len(total_loss), 1))
 final_X = np.hstack((X, scores))
 np.savetxt(final_X, output_dir +'/final_array.txt')
-'''
 
-sample_dir = '/projects/schwartzlabscratch/DesignOpt/test_results/adjustpaired3/0'
-ground_truth_dir = '/projects/schwartzlabscratch/LiquidBiopsyData/training_data/results'
 
-print(getscoresfromcalls(sample_dir, ground_truth_dir))
+#sample_dir = '/projects/schwartzlabscratch/DesignOpt/test_results/072324realruntwo/0'
+#ground_truth_dir = '/projects/schwartzlabscratch/LiquidBiopsyData/training_data/results'
+
+#print(getscoresfromcalls(sample_dir, ground_truth_dir))
